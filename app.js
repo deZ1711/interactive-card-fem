55fdae82648d3d61e3c89f473b0384c9855bc6b9
Benjamin deZ <dev@bdesile.com>
2022-11-23 23:20:11 +0100

    bp pb div>p:absolute

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const cardCvv = document.querySelector('.card__cvv')
-const cardNumber = document.querySelector('.card__number')
-const cardName = document.querySelector('.card__name')
-const cardMonth = document.querySelector('.card__month')
-const cardYear = document.querySelector('.card__year')
-
 const inputName = document.querySelector('.card__form--name')
-const inputNumber = document.querySelector('.card__form--number')
-const inputMonth = document.querySelector('.card__form--month')
-const inputYear = document.querySelector('.card__form--year')
+const cardName = document.querySelector('.card__name')
 const inputCvv = document.querySelector('.card__form--cvv')
+const cardCvv = document.querySelector('.card__cvv')
+const inputNumber = document.querySelector('.card__form--number')
+const cardNumber = document.querySelector('.card__number')
+const inputMonth = document.querySelector('.card__form--month')
+const cardMonth = document.querySelector('.card__month')
+const inputYear = document.querySelector('.card__form--year')
+const cardYear = document.querySelector('.card__year')
+const submitBtn = document.querySelector('.card__button--submit')
+const completedSection = document.querySelector('.card__completed')
+const formSection = document.querySelector('.card__form')
+const resetBtn = document.querySelector('.card__button--continue') 
 
 
 inputName.addEventListener('keyup', function (e) {
@@ -25,4 +28,21 @@ inputYear.addEventListener('keyup', function (e) {
 })
 inputCvv.addEventListener('keyup', function (e) {
   cardCvv.innerText = e.target.value
+})
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  formSection.classList.add('hidden')
+  formSection.setAttribute('aria-hidden', 'true')
+  completedSection.classList.remove('hidden')
+  completedSection.removeAttribute('aria-hidden')
+})
+
+resetBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  completedSection.classList.add('hidden')
+  completedSection.setAttribute('aria-hidden', 'true')
+  formSection.classList.remove('hidden')
+  formSection.removeAttribute('aria-hidden')
+  formSection.reset()
 })
